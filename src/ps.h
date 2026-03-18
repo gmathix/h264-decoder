20924fcf1554a76ac75bf16afeda657206060026
gmathix <smegmuscarlsen@gmail.com>
2026-03-18 12:53:38 +0100

    remove useless shit for now

--- src/ps.h
+++ src/ps.h
@@ -71,36 +71,17 @@ typedef struct PPS {
 
     // RAW (from bitstream)
     int      cabac; // entropy_coding_mode_flag
-    int      bottom_field_pic_order_in_frame_present_flag;
-    uint32_t slice_group_map_type;
-    uint32_t top_left[256];
-    uint32_t bottom_right[256];
-    int      slice_group_change_direction_flag;
-    int32_t  slice_group_id[256];
+    uint32_t pic_init_qp_minus26;
 
-    int      weighted_pref_flag;
-    int      weighted_bipred_idc;
     int32_t  chroma_qp_index_offset;
     int      deblocking_filter_control_present_flag;
     int      constrained_intra_pred_flag;
     int      redundant_pic_cnt_present_flag;
 
-    int      transform_8x8_mode_flag;
-    int      pic_scaling_matrix_present_flag;
-    uint8_t  scaling_matrix4[6][16];
-    uint8_t  scaling_matrix8[6][64];
-    int32_t  second_chroma_qp_index_offset;
-
 
     // DERIVED
-    uint32_t num_slice_groups; // num_slice_groups_minus1 + 1
-    uint32_t slice_group_change_rate; // slice_group_change_rate_minus1 + 1
-    uint32_t pic_size_in_map_units; // pic_size_in_map_units_minus1 + 1
-    uint32_t run_length[256]; // run_length_minus1 + 1
-    int32_t  pic_init_qp; // pic_init_qp_minus26 + 26
-    int32_t  pic_init_qs; // pic_init_qs_minus26 + 26
-    uint32_t num_ref_idx_l0_active; // num_ref_idx_l0_default_active_minus1 + 1
-    uint32_t num_ref_idx_l1_active; // num_ref_idx_l1_default_active_minus1 + 1
+    uint32_t pic_init_qp_minus_26;
+
 
 } PPS ;
 

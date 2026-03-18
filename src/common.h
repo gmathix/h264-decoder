08af51c728542d709c9ded5053d3f862c7e8d423
gmathix <smegmuscarlsen@gmail.com>
2026-03-18 16:28:23 +0100

    added NAL prefix for more clarity

--- src/common.h
+++ src/common.h
@@ -4,52 +4,53 @@
 
 #ifndef TOY_H264_COMMON_H
 #define TOY_H264_COMMON_H
-#include <stddef.h>
+
 #include <stdint.h>
 
 
 /* Table 7.1 */
 enum NalUnitType : uint8_t {
-    UNSPECIFIED = 0,
-    CODED_SLICE_OF_NON_IDR_PICTURE = 1,
-    CODED_SLICE_DATA_PARTITION_A = 2,
-    CODED_SLICE_DATA_PARTITION_B = 3,
-    CODED_SLICE_DATA_PARTITION_C = 4,
-    CODED_SLICE_OF_IDR_PICTURE = 5,
-    SEI = 6,
-    SPS = 7,
-    PPS = 8,
-    AUD = 9,
-    EOSEQ = 10,
-    EOSTREAM = 11,
-    FILLER_DATA = 12,
-    SPS_EXTENSION = 13,
-    PREFIX = 14,
-    SUBSET_SPS = 15,
+    NAL_UNSPECIFIED = 0,
+    NAL_CODED_SLICE_OF_NON_IDR_PICTURE = 1,
+    NAL_CODED_SLICE_DATA_PARTITION_A = 2,
+    NAL_CODED_SLICE_DATA_PARTITION_B = 3,
+    NAL_CODED_SLICE_DATA_PARTITION_C = 4,
+    NAL_CODED_SLICE_OF_IDR_PICTURE = 5,
+    NAL_SEI = 6,
+    NAL_SPS = 7,
+    NAL_PPS = 8,
+    NAL_AUD = 9,
+    NAL_EOSEQ = 10,
+    NAL_EOSTREAM = 11,
+    NAL_FILLER_DATA = 12,
+    NAL_SPS_EXTENSION = 13,
+    NAL_PREFIX = 14,
+    NAL_SUBSET_SPS = 15,
 
     // 16..18 : reserved
-    RS16 = 16,
-    RS17 = 17,
-    RS18 = 18,
+    NAL_RS16 = 16,
+    NAL_RS17 = 17,
+    NAL_RS18 = 18,
 
-    CODED_SLICE_OF_AUX_CODED_PICTURE = 19,
-    CODED_SLICE_EXTENSION = 20,
+    NAL_CODED_SLICE_OF_AUX_CODED_PICTURE = 19,
+    NAL_CODED_SLICE_EXTENSION = 20,
 
     // 21..23 : reserved
-    RS21 = 21,
-    RS22 = 22,
-    RS23 = 23,
+    NAL_RS21 = 21,
+    NAL_RS22 = 22,
+    NAL_RS23 = 23,
 
     // 24..31 : unspecified
-    UNSPEC24 = 24,
-    UNSPEC25 = 25,
-    UNSPEC26 = 26,
-    UNSPEC27 = 27,
-    UNSPEC28 = 28,
-    UNSPEC29 = 29,
-    UNSPEC30 = 30,
-    UNSPEC31 = 31,
-};
+    NAL_UNSPEC24 = 24,
+    NAL_UNSPEC25 = 25,
+    NAL_UNSPEC26 = 26,
+    NAL_UNSPEC27 = 27,
+    NAL_UNSPEC28 = 28,
+    NAL_UNSPEC29 = 29,
+    NAL_UNSPEC30 = 30,
+    NAL_UNSPEC31 = 31,
+} ;
+
 
 char *NalUnitTypeToString(uint8_t nal_unit_type);
 

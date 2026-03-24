298393ee67196dfb43c4b9a015f2aa0e3b9e15c5
gmathix <smegmuscarlsen@gmail.com>
2026-03-24 21:16:54 +0100

    style refactor

--- src/annexb.h
+++ src/annexb.h
@@ -10,9 +10,9 @@
 #include "common.h"
 
 
-int  count_nals(const uint8_t *buf, size_t size);
-void fill_nal_units(const uint8_t *buf, size_t size, NalUnit *p_nalArray, int maxNals);
-uint8_t *nal_to_rbsp(const uint8_t *buf, size_t size, size_t *p_rbspSize);
+int     count_nals     (const uint8_t *buf, size_t size);
+void    fill_nal_units (const uint8_t *buf, size_t size, NalUnit *p_nalArray, int maxNals);
+uint8_t *nal_to_rbsp   (const uint8_t *buf, size_t size, size_t *p_rbspSize);
 
 
 #endif //TOY_H264_ANNEXB_H

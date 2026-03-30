decc5ff4ec91aaabe90a0b7b8c6b1e0b885243d6
gmathix <smegmuscarlsen@gmail.com>
2026-03-30 15:58:10 +0200

    AC + DC residual parser for luma/chroma + stub cavlc reader for now

--- src/mb.c
+++ src/mb.c
@@ -4,6 +4,7 @@
 
 #include "mb.h"
 
+#include <stdlib.h>
 #include <threads.h>
 #include <unistd.h>
 
@@ -93,7 +94,7 @@ const B_MbInfo b_mb_type_info[23] = {
 
 /* table 7-18 */
 const B_MbInfo b_sub_mb_type_info[13] = {
-    /*  0 */ {SUB_MB_TYPE_DIRECT | SUB_MB_TYPE_8x8 | PRED_DIRECT, 4, 4, 4},
+    /*  0 */ {SUB_MB_TYPE_DIRECT | SUB_MB_TYPE_8x8 | PRED_DIRECT,    4, 4, 4},
     /*  1 */ {SUB_MB_TYPE_8x8    | MB_TYPE_P0L0,                     1, 8, 8},
     /*  2 */ {SUB_MB_TYPE_8x8    | MB_TYPE_P0L1,                     1, 8, 8},
     /*  3 */ {SUB_MB_TYPE_8x8    | MB_TYPE_P0L0 | MB_TYPE_P0L1,      1, 8, 8},
@@ -110,6 +111,15 @@ const B_MbInfo b_sub_mb_type_info[13] = {
 };
 
 
+/* table 6-1 */
+const int sub_width_c_info[4]  = {
+    -1, /* monochrome */
+     2, /*   4:2:0    */
+     2, /*   4:2:2    */
+     1, /*   4:4:4    */
+};
+const int sub_height_c_info[4] = {-1, 2, 1, 1};
+
 
 /* 7.3.5 */
 void decode_macroblock(SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps) {
@@ -118,11 +128,24 @@ void decode_macroblock(SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
 
     uint32_t mb_type = read_ue(br);
 
+    if ((IS_I_SLICE(s_h->slice_type) && mb_type > 25) ||
+        (IS_P_SLICE(s_h->slice_type) && mb_type > 4)  ||
+        (IS_B_SLICE(s_h->slice_type) && mb_type > 22)) {
+
+        printf("mb_type out of bounds for %s slice : got %d\n",
+            slice_type_to_string(s_h->slice_type), mb_type);
+        return;
+    }
+
+    int MbWidthC  = 16 / sub_width_c_info[s_h->sps->chroma_format_idc];
+    int MbHeightC = 16 / sub_height_c_info[s_h->sps->chroma_format_idc];
+
+
     uint8_t pcm_samples_luma[256];
-    uint8_t pcm_samples_chroma[256];
+    uint8_t pcm_samples_chroma[2 * MbHeightC * MbWidthC];
 
 
-    uint8_t type = IS_I_SLICE(s_h->slice_type)
+    int type = IS_I_SLICE(s_h->slice_type)
         ? i_mb_type_info[mb_type].type
         : IS_P_SLICE(s_h->slice_type)
             ? p_mb_type_info[mb_type].type
@@ -130,7 +153,7 @@ void decode_macroblock(SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
                 ? b_mb_type_info[mb_type].type
                 : -1;
 
-    uint8_t pred_mode = IS_I_SLICE(s_h->slice_type)
+    int pred_mode = IS_I_SLICE(s_h->slice_type)
         ? i_mb_type_info[mb_type].pred_mode
         : -1;
 
@@ -154,6 +177,9 @@ void decode_macroblock(SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
         for (int i = 0; i < 256; i++) {
             pcm_samples_luma[i] = read_u(br, 8);
         }
+        for (int i = 0; i < 2 * MbHeightC * MbWidthC; i++) {
+            pcm_samples_chroma[i] = read_u(br, 8);
+        }
     } else {
         int transform_size_8x8_flag = 0;
         int noSubMbPartSizeLessThan8x8Flag = 1;
@@ -163,33 +189,42 @@ void decode_macroblock(SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
             if (pps->transform_8x8_mode_flag && type == MB_TYPE_INTRA4x4) {
                 transform_size_8x8_flag = read_u(br, 1);
             }
-            mb_pred(type, s_h, nal_unit, br, ps);
+            mb_pred(type, pred_mode, s_h, nal_unit, br, ps);
         }
 
         if (!IS_INTRA16x16(type)) {
             int32_t cbp = map_coded_block_pattern(read_ue(br), sps->chroma_format_idc,
                 IS_INTRA4x4(type));
-            printf("cbp:%d", cbp);
+
+            cbp_luma   = cbp % 16;
+            cbp_chroma = cbp / 16;
 
             if (cbp_luma > 0 && transform_size_8x8_flag &&
                 !IS_INTRA4x4(type) && noSubMbPartSizeLessThan8x8Flag &&
                 (type != (MB_TYPE_DIRECT2 | MB_TYPE_L0L1) || sps->direct_8x8_inference_flag)) {
 
-                int transform_size_8x8_flag = read_u(br, 1);
+                transform_size_8x8_flag = read_u(br, 1);
             }
         }
 
-        if (IS_INTRA16x16(type)) {
+        if (cbp_luma > 0 || cbp_chroma > 0 || IS_INTRA16x16(type)) {
             int32_t mb_qp_delta = read_se(br);
-            printf("ASDFASDF\n");
-            /* residual (0, 15) */
+
+            residual_function residual_block = s_h->pps->entropy_coding_mode_flag
+                ? &residual_block_cabac
+                : &residual_block_cavlc;
+
+            printf("cbp_luma:%d cbp_chroma:%d", cbp_luma, cbp_chroma);
+
+            residual(type, transform_size_8x8_flag, 0, 15, cbp_luma, cbp_chroma,
+                        residual_block, br, s_h);
         }
     };
 }
 
 
 /* 7.3.5.1 */
-void mb_pred(int type, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps) {
+void mb_pred(int type, int pred_mode, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps) {
     if (type == MB_TYPE_INTRA4x4 ||
         type == MB_TYPE_INTRA8x8 ||
         type == MB_TYPE_INTRA16x16) {
@@ -208,7 +243,7 @@ void mb_pred(int type, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
         if (type == MB_TYPE_INTRA8x8) {
             uint8_t prev_intra8x8_pred_mode_flag[16];
             uint8_t rem_intra8x8_pred_mode[16];
-            for (int luma8x8BlkIdx = 0; luma8x8BlkIdx < 16; luma8x8BlkIdx++) {
+            for (int luma8x8BlkIdx = 0; luma8x8BlkIdx < 4; luma8x8BlkIdx++) {
                 prev_intra8x8_pred_mode_flag[luma8x8BlkIdx] = read_u(br, 1);
                 if (!prev_intra8x8_pred_mode_flag[luma8x8BlkIdx]) {
                     rem_intra8x8_pred_mode[luma8x8BlkIdx] = read_u(br, 3);
@@ -225,11 +260,131 @@ void mb_pred(int type, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, Param
 }
 
 
-/* 7.3.5.2 */
-void residual(int startIdx, int endIdx, SliceHeader *s_h, BitReader *br, ParamSets *ps) {
-    /* fuck the table is too scary and it's 3 AM so i'll just fucking go to bed and do this shit tomorrow
-     * why do i fucking need to implement a fucking 500 line grammar parser which i 99% dont need for now
-     * just to fucking not desync because of the fucking 16x16 intra blocks
-     * future me, if you made it past this project, i'm sofucking proud of you rn */
+/* 7.3.5.3 */
+void residual(int type, int t_8x8_flag, int startIdx, int endIdx, int cbp_luma, int cbp_chroma, residual_function residual_block, BitReader *br,  SliceHeader *s_h) {
+
+
+    int16_t intra16x16DC     [16];  /* one DC coeff for each 4x4 luma block */
+    int16_t intra16x16AC [16][15];  /* 15 AC coeffs (1 4x4 block) for each 4x4 luma block for Intra16x16*/
+    int16_t luma4x4      [16][16];  /* 16 AC coeffs (1 4x4 block) for each 4x4 luma block */
+    int16_t luma8x8      [ 4][64];
+
+    residual_luma(type, t_8x8_flag, cbp_luma, startIdx, endIdx,
+                    intra16x16DC, intra16x16AC, luma4x4, luma8x8,
+                    residual_block, br, s_h);
+
+
+
+    if (s_h->sps->chroma_format_idc == 1 || s_h->sps->chroma_format_idc == 2) {
+
+        int numC8x8 = 4 /
+            (sub_width_c_info[s_h->sps->chroma_format_idc] * sub_height_c_info[s_h->sps->chroma_format_idc]);
+
+
+        ResidualScratch residual_scratch = {};
+
+        for (int iCbCr = 0; iCbCr < 2; iCbCr++) {
+            if ((cbp_chroma & 3) && startIdx == 0) {
+                /* chroma DC residual present */
+                (*residual_block)(residual_scratch.chromaDC[iCbCr], 0, 4*numC8x8-1, 4*numC8x8, br, s_h);
+            } else {
+                for (int i = 0; i < 4 * numC8x8; i++) {
+                    residual_scratch.chromaDC[iCbCr][i] = 0;
+                }
+            }
+        }
+
+        for (int iCbCr = 0; iCbCr < 2; iCbCr++) {
+            for (int i8x8 = 0; i8x8 < numC8x8; i8x8++) {
+                for (int i4x4 = 0; i4x4 < 4; i4x4++) {
+                    if (cbp_chroma & 2) {
+                        /* chroma AC residual present */
+                        (*residual_block)(residual_scratch.chromaAC[iCbCr][i8x8*4 + i4x4],
+                            _max(0, startIdx-1), endIdx-1, 15, br, s_h);
+                    } else {
+                        for (int i = 0; i < 15; i++) {
+                            residual_scratch.chromaAC[iCbCr][i8x8*4 + i4x4][i] = 0;
+                        }
+                    }
+                }
+            }
+        }
+    } else if (s_h->sps->chroma_format_idc == 3) {
+        int16_t CbIntra16x16DC[16];
+        int16_t CbIntra16x16AC[16][15];
+        int16_t Cb4x4[16][16];
+        int16_t Cb8x8[4][64];
+        residual_luma(type, t_8x8_flag, cbp_chroma, startIdx,  endIdx,
+                         CbIntra16x16DC, CbIntra16x16AC, Cb4x4, Cb8x8,
+                         residual_block, br, s_h);
+
+        int16_t CrIntra16x16DC[16];
+        int16_t CrIntra16x16AC[16][15];
+        int16_t Cr4x4[16][16];
+        int16_t Cr8x8[4][64];
+        residual_luma(type, t_8x8_flag, cbp_chroma, startIdx,  endIdx,
+                        CrIntra16x16DC, CrIntra16x16AC, Cr4x4, Cr8x8,
+                        residual_block, br, s_h);
+    }
+}
+
+
+
+/* 7.3.5.3.1 */
+void residual_luma(int type, int t_8x8_flag, int cbp_luma, int startIdx, int endIdx,
+                    int16_t i16x16DClevel[16], int16_t i16x16AClevel[16][15], int16_t level4x4[16][16], int16_t level8x8[4][64],
+                    residual_function residual_block, BitReader *br, SliceHeader *s_h) {
+
+    if (startIdx == 0 && IS_INTRA16x16(type)) {
+        printf("\n parsing DC luma coeffs\n");
+        (*residual_block)(i16x16DClevel, 0, 15, 16, br, s_h);
+    }
+
+    for (int i8x8 = 0; i8x8 < 4; i8x8++) {
+        if (!t_8x8_flag || !s_h->pps->entropy_coding_mode_flag) {
+            for (int i4x4 = 0; i4x4 < 4; i4x4++) {
+                if (cbp_luma & (1 << i8x8)) {
+                    if (IS_INTRA16x16(type)) {
+                        (*residual_block)(i16x16AClevel[i8x8*4 + i4x4],
+                            _max(0, startIdx - 1), endIdx - 1, 15, br, s_h);
+                    } else {
+                        (*residual_block)(level4x4[i8x8*4 + i4x4],
+                            startIdx, endIdx, 16, br, s_h);
+                    }
+                } else if (IS_INTRA16x16(type)) {
+                    for (int i = 0; i < 15; i++) {
+                        i16x16AClevel[i8x8*4 + i4x4][i] = 0;
+                    }
+                } else {
+                    for (int i = 0; i < 16; i++) {
+                        level4x4[i8x8*4 + i4x4][i] = 0;
+                    }
+                }
+
+                if (!s_h->pps->entropy_coding_mode_flag && t_8x8_flag) {
+                    for (int i = 0; i < 16; i++) {
+                        level8x8[i8x8][4*i + i4x4] = level4x4[i8x8*4 + i4x4][i];
+                    }
+                }
+            }
+        } else if (cbp_luma & (1 << i8x8)) {
+            (*residual_block)(level8x8[i8x8], 4*startIdx, 4*endIdx+3, 64, br, s_h);
+        } else {
+            for (int i = 0; i < 64; i++) {
+                level8x8[i8x8][i] = 0;
+            }
+        }
+    }
+}
+
+
+/* 7.3.5.3.2 */
+void residual_block_cavlc(int16_t coeffLevel[], int startIdx, int endIdx, int maxNumCoeff, BitReader *br, SliceHeader *s_h) {
+
+}
+
+
+/* 7.3.5.3.3 */
+void residual_block_cabac(int16_t coeffLevel[], int startIdx, int endIdx, int maxNumCoeff, BitReader *br, SliceHeader *s_h) {
 
 }
--- src/mb.h
+++ src/mb.h
@@ -14,8 +14,8 @@
 
 
 typedef struct I_MbInfo {
-    uint32_t type;
-    uint8_t pred_mode;
+    int type;
+    int pred_mode;
     int cbp_chroma;
     int cbp_luma;
 } I_MbInfo ;
@@ -24,7 +24,7 @@ extern const I_MbInfo i_mb_type_info[26];
 
 
 typedef struct P_MbInfo {
-    uint32_t type;
+    int type;
     uint8_t  part_count;
     uint8_t  mb_part_width;
     uint8_t  mb_part_height;
@@ -35,8 +35,8 @@ extern const P_MbInfo p_sub_mb_type_info[4];
 
 
 typedef struct B_MbInfo {
-    uint32_t type;
-    uint8_t part_count;
+    int type;
+    uint8_t  part_count;
     uint8_t  mb_part_width;
     uint8_t  mb_part_height;
 } B_MbInfo ;
@@ -44,11 +44,92 @@ typedef struct B_MbInfo {
 extern const B_MbInfo b_mb_type_info[23];
 extern const B_MbInfo b_sub_mb_type_info[13];
 
+extern const int sub_width_c_info[4];
+extern const int sub_height_c_info[4];
+
+
+typedef struct {
+    int16_t chromaDC[2][4];     // FIXME: no assumption for 4:2:0 and use [2][8] for 4:2:2
+    int16_t chromaAC[2][4][15]; // FIXME: same, [2][8][15] for 4:2:2
+} ResidualScratch ;
+
+
+typedef void (*residual_function)(int16_t[], int, int, int, BitReader*, SliceHeader*);
+
 
 
 void  decode_macroblock        (SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps);
-void  mb_pred                  (int type, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps);
-void  residual                 (int startIdx, int endIdx, SliceHeader *s_h, BitReader *br, ParamSets *ps)
+void  mb_pred                  (int type, int pred_mode, SliceHeader *s_h, NalUnit *nal_unit, BitReader *br, ParamSets *ps);
+
+void  residual                 (int type, int t_8x8_flag, int startIdx, int endIdx, int cbp_luma, int cbp_chroma,
+                                    residual_function residual_block,  BitReader *br, SliceHeader *s_h);
+
+void  residual_luma            (int type, int t_8x8_flag, int cbp_luma, int startIdx, int endIdx,
+                                    int16_t i16x16[16], int16_t i16x16AC[16][15], int16_t level4x4[16][16], int16_t level8x8[4][64],
+                                    residual_function residual_block, BitReader *br, SliceHeader *s_h);
+
+void  residual_block_cavlc     (int16_t coeffLevel[], int startIdx, int endIdx, int maxNumCoeff, BitReader *br, SliceHeader *s_h);
+void  residual_block_cabac     (int16_t coeffLevel[], int startIdx, int endIdx, int maxNumCoeff, BitReader *br, SliceHeader *s_h);
+
+
+
+/* little luma/chroma coeff tutorial */
+
+/*
+
+*** LUMA BLOCKS INSIDE A MACROBLOCK ***
+
+    a macroblock is 16x16 luma -> 16 x (4x4 blocks)
+
+    layout:
+
+    [ 0] [ 1] [ 2] [ 3]
+    [ 4] [ 5] [ 6] [ 7]    -> each of those blocks has its own transform coeffs
+    [ 8] [ 9] [10] [11]       so residual parsing fills int16_t coeff[16][16]
+    [12] [13] [14] [15]
+
+
+    transform coeffs are not coded row-by-row but scanned in zigzag order, for example
+
+     0  1  4  5
+     2  3  6  7    -> CAVLC decodes coeff[zigzag[i]]
+     8  9 12 13
+    10 11 14 15
+
+
+
+*** DC vs AC COEFFICIENTS ***
+
+    in every transform block : [0][0] = DC
+    this is the average intensity of the residual block
+    everything else : AC coefficients represent higher spatial frequencies
+
+    DC is treated specially because neighbor prediction already removes DC energy
+    but for Intra16x16, DC is handled differently :
+
+        instead of 16 separate DC values :
+           1. each 4x4 block still has AC coefficients
+           2. the 16 DC values are grouped
+           3. a secondary 4x4 transform is applied to those DCs
+
+        so you parse 16 AC blocks + 1 DC block (16 coeffs)
+        that DC block goes through a Hadamard transform, not the normal one
+
+
+*** CHROMA RESIDUALS ***
+
+chroma blocks depend on subsampling
+for the most common case 4:2:0, 1 luma macroblock correponds to 2 chroma planes (Cb, Cr)
+   each of size 8x8, split into 4 x (4x4 blocks)
+
+so per macroblock : 4 Cb plane blocks, 4 Cr plane blocks
+each has AC blocks, plus a 2x2 DC block
+
+
+*/
+
+
+
 
 static inline int inverse_mb_scan_x          (uint32_t mbAddr, SPS *sps) {
     return _inverse_raster_scan(

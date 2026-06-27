69381cb2401be88db21f3c66679e511e71b4879d
gmathix <smegmuscarlsen@gmail.com>
2026-06-27 09:49:22 +0200

    make everything static

--- src/util/formulas.h
+++ src/util/formulas.h
@@ -15,17 +15,17 @@
 
 
 
-ALWAYS_INLINE int32_t  _abs(int32_t x)           { return x >= 0 ? x : -x; }
-ALWAYS_INLINE int32_t _min(int32_t x, int32_t y) { return x <= y ? x : y; }
-ALWAYS_INLINE int32_t _max(int x, int y) { return x >= y ? x : y; }
-ALWAYS_INLINE double  _log2(double x)          { return log2(x); }
-ALWAYS_INLINE double  _log10(double x)         { return log10(x); }
-ALWAYS_INLINE double  _sqrt(double x)          { return sqrt(x); }
+static ALWAYS_INLINE int32_t  _abs(int32_t x)           { return x >= 0 ? x : -x; }
+static ALWAYS_INLINE int32_t _min(int32_t x, int32_t y) { return x <= y ? x : y; }
+static ALWAYS_INLINE int32_t _max(int x, int y) { return x >= y ? x : y; }
+static ALWAYS_INLINE double  _log2(double x)          { return log2(x); }
+static ALWAYS_INLINE double  _log10(double x)         { return log10(x); }
+static ALWAYS_INLINE double  _sqrt(double x)          { return sqrt(x); }
 
-ALWAYS_INLINE int32_t _floor(double x)         { return (int32_t) x; }
-ALWAYS_INLINE int32_t _ceil(double x)          { return (int32_t)x + (x > (int32_t)x ? 1 : 0); }
-ALWAYS_INLINE int32_t _sign(double x)          { return x >= 0 ? 1 : -1; }
-ALWAYS_INLINE int32_t _round(double x)         { return _sign(x) * _floor(_abs(x) + 0.5); }
+static ALWAYS_INLINE int32_t _floor(double x)         { return (int32_t) x; }
+static ALWAYS_INLINE int32_t _ceil(double x)          { return (int32_t)x + (x > (int32_t)x ? 1 : 0); }
+static ALWAYS_INLINE int32_t _sign(double x)          { return x >= 0 ? 1 : -1; }
+static ALWAYS_INLINE int32_t _round(double x)         { return _sign(x) * _floor(_abs(x) + 0.5); }
 
 
 static ALWAYS_INLINE int32_t rshift_min(int32_t n, int16_t qp) {
@@ -49,36 +49,36 @@ static void binprintf(int v, int length)
 }
 
 
-ALWAYS_INLINE int32_t _clip3(int32_t x, int32_t y, int32_t z) {
+static ALWAYS_INLINE int32_t _clip3(int32_t x, int32_t y, int32_t z) {
     if (z < x) return x;
     if (z > y) return y;
     return z;
 }
 
-ALWAYS_INLINE int32_t _clip2(int32_t x, int32_t y) {
+static ALWAYS_INLINE int32_t _clip2(int32_t x, int32_t y) {
     return y > x ? y : x;
 }
 
-ALWAYS_INLINE int32_t _clip1y(int32_t x, int bit_depth) {
+static ALWAYS_INLINE int32_t _clip1y(int32_t x, int bit_depth) {
     return _clip3(0, (1 << bit_depth) - 1, x);
 }
 
-ALWAYS_INLINE int32_t _clip1c(int32_t x, CodecContext *ctx) {
-    return _clip3(0, (1 << ctx->ps->sps->bit_depth_chroma) - 1, x);
+static ALWAYS_INLINE int32_t _clip1c(int32_t x, int bit_depth) {
+    return _clip3(0, (1 << bit_depth) - 1, x);
 }
 
-ALWAYS_INLINE int32_t _inverse_raster_scan(int32_t a, int32_t b, int32_t c, int32_t d, int32_t e) {
+static ALWAYS_INLINE int32_t _inverse_raster_scan(int32_t a, int32_t b, int32_t c, int32_t d, int32_t e) {
     return e == 0
         ? (a % (d / b)) * b
         : (a / (d / b)) * c;
 }
 
-ALWAYS_INLINE int _median(int x, int y, int z) {
+static ALWAYS_INLINE int _median(int x, int y, int z) {
     return x + y + z - _min(x, _min(y, z)) - _max(x, _max(y, z));
 }
 
 
-ALWAYS_INLINE void nothing() { void *nothing = NULL; }
+static ALWAYS_INLINE void nothing() { void *nothing = NULL; }
 
 
 

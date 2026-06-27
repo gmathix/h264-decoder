f3c116432bdcf66edddae07b5ec07f721b754eb8
gmathix <smegmuscarlsen@gmail.com>
2026-06-27 09:51:26 +0200

    fix chroma inter prediction

--- src/inter.c
+++ src/inter.c
@@ -7,7 +7,7 @@
 
 
 /* gets called for every 4x4 sub macroblock every time, even if they belong to a 16x16 inter mb
- * pos: 4x4 block index within macroblock
+ * TODO: adaptive inter pred following mb partitioning (avoid calling this 16 times for a 16x16 mb when inter pred could be done in one shot)
  */
 void inter_pred(Macroblock *mb, int idx, MotionVector *mv, CodecContext *ctx) {
 
@@ -18,7 +18,7 @@ void inter_pred(Macroblock *mb, int idx, MotionVector *mv, CodecContext *ctx) {
     int yBase = mb->mb_y*16 + ((idx>>2) << 2);
     int xBase = mb->mb_x*16 + ((idx&3)  << 2);
 
-    int stride = currPic->strideY;
+    int stride = currPic->widthY;
     uint8_t *dst = &currPic->luma[yBase*stride + xBase];
 
     // mv offsets
@@ -27,45 +27,45 @@ void inter_pred(Macroblock *mb, int idx, MotionVector *mv, CodecContext *ctx) {
     int xFrac    = mv->x & 3;
     int yFrac    = mv->y & 3;
 
-    uint8_t ref_samples[9][9];
-    fetch_9x9_block(refPic, yBase + yOffInt, xBase + xOffInt, ref_samples, ctx);
 
-    qpel_funcs[(yFrac<<2) | xFrac] (ref_samples, dst, stride, ctx->ps->sps->bit_depth_luma_minus8 + 8);
+    fetch_9x9_block(refPic, yBase + yOffInt, xBase + xOffInt, ctx->ref_samples, ctx);
+
+    qpel_funcs[(yFrac<<2) | xFrac] (ctx->ref_samples, dst, stride, ctx->ps->sps->bit_depth_luma_minus8 + 8);
     // qpel_00(ref_samples, dst, stride, ctx->ps->sps->bit_depth_luma_minus8 + 8); // fun testing
 }
 
-void inter_pred_chroma(Macroblock *mb, int idx, MotionVector *mv, CodecContext *ctx) {
+void inter_pred_chroma(Macroblock *mb, int y, int x, MotionVector *mv, CodecContext *ctx) {
     Picture *currPic = mb->p_pic;
     Picture *refPic  = ctx->dpb->l0[mv->ref_idx];
 
-    int yBase = mb->mb_y*8 + ((idx>>1) << 2);
-    int xBase = mb->mb_x*8 + ((idx&1)  << 2);
+    const int yBase = mb->mb_y*8 + y;
+    const int xBase = mb->mb_x*8 + x;
 
-    int stride = currPic->strideC;
+    int stride = currPic->widthC;
     uint8_t *dstCb = &currPic->cb[yBase*stride + xBase];
     uint8_t *dstCr = &currPic->cr[yBase*stride + xBase];
 
-    int xOffInt  = mv->x >> 3;
-    int yOffInt  = mv->y >> 3;
-    int xFrac    = mv->x & 7;
-    int yFrac    = mv->y & 7;
+    const int xOffInt  = mv->x >> 3;
+    const int yOffInt  = mv->y >> 3;
+    const int xFrac    = mv->x & 7;
+    const int yFrac    = mv->y & 7;
 
-    uint8_t ref_cb[5][5], ref_cr[5][5];
-    fetch_5x5_block_chroma(refPic, yBase + yOffInt, xBase + xOffInt, ref_cb, ref_cr, ctx);
+    uint8_t ref_cb[3][3], ref_cr[3][3];
+    fetch_3x3_block_chroma(refPic, yBase + yOffInt, xBase + xOffInt, ref_cb, ref_cr, ctx);
 
-    for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-            *(dstCb++) = ((8-xFrac) * (8-yFrac) * ref_cb[y][x]     +
-                             xFrac  * (8-yFrac) * ref_cb[y][x+1]   +
-                          (8-xFrac) *    yFrac  * ref_cb[y+1][x]   +
-                             xFrac  *    yFrac  * ref_cb[y+1][x+1] + 32) >> 6;
-            *(dstCr++) = ((8-xFrac) * (8-yFrac) * ref_cr[y][x]     +
-                             xFrac  * (8-yFrac) * ref_cr[y][x+1]   +
-                          (8-xFrac) *    yFrac  * ref_cr[y+1][x]   +
-                             xFrac  *    yFrac  * ref_cr[y+1][x+1] + 32) >> 6;
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            dstCb[j] = ((8-xFrac) * (8-yFrac) * ref_cb[i][j]     +
+                           xFrac  * (8-yFrac) * ref_cb[i][j+1]   +
+                        (8-xFrac) *    yFrac  * ref_cb[i+1][j]   +
+                           xFrac  *    yFrac  * ref_cb[i+1][j+1] + 32) >> 6;
+            dstCr[j] = ((8-xFrac) * (8-yFrac) * ref_cr[i][j]     +
+                           xFrac  * (8-yFrac) * ref_cr[i][j+1]   +
+                        (8-xFrac) *    yFrac  * ref_cr[i+1][j]   +
+                           xFrac  *    yFrac  * ref_cr[i+1][j+1] + 32) >> 6;
         }
-        dstCb += stride - 4;
-        dstCr += stride - 4;
+        dstCb += stride;
+        dstCr += stride;
     }
 }
 

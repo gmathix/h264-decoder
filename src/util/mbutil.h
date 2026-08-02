e913ef4a8464f355c3afb31e2a334b646f154e35
gmathix <smegmuscarlsen@gmail.com>
2026-08-02 16:35:08 +0200

    include I_PCM in IS_INTRA()

--- src/util/mbutil.h
+++ src/util/mbutil.h
@@ -67,7 +67,7 @@
 #define IS_INTRANxN(a)     ((a) & (MB_TYPE_INTRA4x4 | MB_TYPE_INTRA8x8))
 #define IS_L0(a)           ((a) & MB_TYPE_L0)
 #define IS_PCM(a)          ((a) & MB_TYPE_INTRA_PCM)
-#define IS_INTRA(a)        ((a) & 7)
+#define IS_INTRA(a)        ((a) & 15)
 #define IS_INTER(a)        ((a) & (MB_TYPE_16x16 | MB_TYPE_16x8 | \
                                    MB_TYPE_8x16  | MB_TYPE_8x8 | \
                                    MB_TYPE_SKIP | MB_TYPE_DIRECT))
